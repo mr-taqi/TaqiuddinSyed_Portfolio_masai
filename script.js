e3f057b781a7efbce3778248a71e2b3b7e39c431
taqiuddin-txp <taqiuddin.syed@travelxp.tv>
2025-06-07 10:52:00 +0530

    refactor : portfolio updated

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function NewTab() {
     window.open(
-    "https://drive.google.com/file/d/1gd13zlOLLnjMu4NXTbjFzQ4Bko2LyWTe/view?usp=sharing",
+    "https://drive.google.com/file/d/1I0z7WCiTyh0X3JawTO75MRDwlcL06JsQ/view?usp=sharing",
     "_blank"
     );
   }
